ef87bbd0bee7b108dbd2c82e0b1d9d0f579cc133
Maxim Balashov <mbalashov@diasoft.ru>
2025-03-23 11:50:01 +0300

    Iter4 done

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -7,23 +7,23 @@ import (
 	metricgenerator "github.com/Maxim-Ba/metriccollector/internal/agent/metric_generator"
 )
 
-const pollInterval = 2 * time.Second
-const reportInterval = 10 * time.Second
+
 
 func main() {
-	httpClient := client.NewClient("localhost", 8080)
+	parseFlags()
+	httpClient := client.NewClient(flagRunAddr)
 	reportIntervalStart := time.Now()
 	for {
 		metrics, err := metricgenerator.Generator.Generate()
 		if err != nil {
 			panic("Can not collect metrics")
 		}
-		if time.Since(reportIntervalStart) >= reportInterval {
+		if time.Since(reportIntervalStart) >= time.Duration(flagReportInterval)* time.Second {
 			metricgenerator.Generator.UpdatePollCount()
 			httpClient.SendMetrics(metrics)
 			reportIntervalStart = time.Now()
 
 		}
-		time.Sleep(pollInterval)
+		time.Sleep(time.Duration(flagPollInterval) * time.Second)
 	}
 }
